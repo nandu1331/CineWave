047d6bf2c56ae8f6ffba14655937ac3901dbe76c
nanda <nandamonsterking@gmail.com>
2024-11-25 17:31:18 +0530

    backend configs

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = [
     '.vercel.app',
     'localhost',
     '127.0.0.1',
+    'https://cine-wave-backend.vercel.app'
 ]
 
 
@@ -142,9 +143,33 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000", 
-    "https://cine-wave-livid.vercel.app" 
+    "https://cine-wave-livid.vercel.app",
+    "http://localhost:3000"
 ]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
 
 # Configure REST Framework
 REST_FRAMEWORK = {
