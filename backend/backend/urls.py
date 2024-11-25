373c823d7f563197b29205a20df6a8ec3d3ce548
nanda <nandamonsterking@gmail.com>
2024-11-25 18:05:37 +0530

    handled default url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,8 +5,10 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from api.views import TestView
 
 urlpatterns = [
+    path('', TestView.as_view(), name='root'),
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
